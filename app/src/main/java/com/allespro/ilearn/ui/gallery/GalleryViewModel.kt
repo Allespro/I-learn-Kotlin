b28e253dde3e8734132c9b773065552c46489437
Allespro <arcy.all@yahoo.com>
2020-07-13 01:33:57 +0300

    I learn something new

--- app/src/main/java/com/allespro/ilearn/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/allespro/ilearn/ui/gallery/GalleryViewModel.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
 
+
+
+
+class GalleryViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "Here would be threads, I want sleep"
     }
     val text: LiveData<String> = _text
 }
